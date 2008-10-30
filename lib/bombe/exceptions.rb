27f7093a1654708a2c9f7104471092cc7c4807a9
flame <flame@yamato.(none)>
2008-10-30 22:58:49 +0100

    Implement exceptions for Backend::Base#seek.

--- lib/bombe/exceptions.rb
+++ lib/bombe/exceptions.rb
@@ -16,5 +16,38 @@
 # <http://www.gnu.org/licenses/>.
 
 module Bombe
-  
+  # Invalid seek exception; this exception is thrown when a seek is
+  # requested that cannot be enacted, like:
+  #
+  # * absolute seek to negative value;
+  # * relative seek that would move before the start of data;
+  # * termination seek that would move before the start of data;
+  # * termination seek on backends not supporting the size primitive.
+  class InvalidSeek < Exception
+    attr_reader :message, :amount, :whence, :pos
+    def initialize(amount, whence, pos = nil)
+      @amount = amount
+      @whence = whence
+      @pos = pos
+
+      if amount
+        @message = "Invalid seek #{amount} of whence #{whence}"
+        @message += " (at #{pos})" if pos
+      else
+        # The only invalid seek with no amount is when the backend
+        # does not support the size function.
+        @message = "Invalid seek of whence #{whence}"
+      end
+    end
+  end
+
+  # Invalid whence exception; this exception is thrown when a seek is
+  # requested with an invalid whence value.
+  class InvalidWhence < Exception
+    attr_reader :message, :whence
+    def initialize(whence)
+      @whence = whence
+      @message = "Invalid whence #{whence} for seek"
+    end
+  end
 end
